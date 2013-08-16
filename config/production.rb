fa57b295114047fba19dd83bb376a7a2a088d0b4
Jason Lee <huacnlee@gmail.com>
2013-08-16 18:12:30 +0800

    -

--- config/production.rb
+++ config/production.rb
@@ -1,8 +1,8 @@
 configure do
-  set :port, $PORT
+  set :port, 3000
   ## worker number can be detected by CPU count
   set :workers, 4
-  set :logger, true
+  set :logger, false
 
   # todo after_fork
 end
