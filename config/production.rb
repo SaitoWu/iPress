20603fc3d4adcfafa7723530317625c737089560
Jason Lee <huacnlee@gmail.com>
2013-08-16 22:06:15 +0800

    Support -p to custom port with production.

--- config/production.rb
+++ config/production.rb
@@ -1,5 +1,17 @@
+require 'optparse'
+args ||= []
+opts = {
+  port: 3000
+}
+OptionParser.new do |opt|
+  opt.banner = 'Usage'
+  opt.on('-p', 'Start port') do |value|
+    opts[:port] = value
+  end
+end.parse(args)
+
 configure do
-  set :port, 3000
+  set :port, opts[:port]
   ## worker number can be detected by CPU count
   set :workers, 4
   set :logger, false
